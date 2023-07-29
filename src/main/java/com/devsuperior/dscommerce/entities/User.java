fb82d30ec2c448791e7c6c723bd57c1f0910300b
Diego Alves <djalves424@gmail.com>
2023-07-29 00:16:29 -0300

    Atualizando a classe User

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -76,4 +76,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthDate=" + birthDate +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
