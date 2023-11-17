75f72b7770fbd1d420ce993d572e007be1d65a5e
djalves424 <djalves424@gmail.com>
2023-11-16 23:34:25 -0300

    GET user logged

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	@Query(nativeQuery = true, value = """
-				SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
-				FROM tb_user
-				INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
-				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
-				WHERE tb_user.email = :email
-			""")
-	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+    @Query(nativeQuery = true, value = """
+            	SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+            	FROM tb_user
+            	INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+            	INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+            	WHERE tb_user.email = :email
+            """)
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
