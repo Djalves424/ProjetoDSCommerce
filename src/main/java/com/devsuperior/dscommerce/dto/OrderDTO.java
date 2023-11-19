0fe25801401db01e68d79a345e4cf72147ea8566
djalves424 <djalves424@gmail.com>
2023-11-18 22:30:49 -0300

    Save Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class OrderDTO {
     private ClientDTO client;
     private PaymentDTO payment;
 
+    @NotEmpty(message = "Deve ter pelo menos um item")
     private List<OrderItemDTO> items = new ArrayList<>();
 
     public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
--- src/main/java/com/devsuperior/dscommerce/entities/OrderStatus.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderStatus.java
@@ -2,5 +2,5 @@ package com.devsuperior.dscommerce.entities;
 
 public enum OrderStatus {
 
-    WAITING, PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+    WAITING_PAYMENT, PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
 }
