67b98a9f3ba942598b05e2c28c070ed81f695111
Diego Alves <djalves424@gmail.com>
2024-02-16 19:52:26 -0300

    Creating unit tests for the OrderServiceTests class

--- src/test/java/com/devsuperior/dscommerce/tests/UserFactory.java
+++ src/test/java/com/devsuperior/dscommerce/tests/UserFactory.java
@@ -26,7 +26,7 @@ public class UserFactory {
 
     }
 
-    public static User createCustomAminUser(Long id, String username) {
+    public static User createCustomAdminUser(Long id, String username) {
         User user = new User(id, "Alex", username, "977777777", LocalDate.parse("1987-12-13"), "$2a$10$SwTf6CVcgfntYt/06tifMey0lAeU6Mkm1Na0c.xCJSjDdj1oFtLRm");
         user.addRole(new Role(2L, "ROLE_ADMIN"));
         return user;
